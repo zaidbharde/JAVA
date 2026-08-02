4ec2fbc5c40acbc4eb0cff201d196661ce1da425
ZAID BHARDE <zaidbharde09@gmail.com>
2026-08-02 23:19:33 +0530

    Create TrappingRainWater.java

---
+++ TrappingRainWater.java
@@ -0,0 +1,24 @@
+public class TrappingRainWater {
+    public static int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0, water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                water += leftMax - height[left];
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                water += rightMax - height[right];
+                right--;
+            }
+        }
+        return water;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println("Trapped water: " + trap(height));
+    }
+}
