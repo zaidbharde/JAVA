74fe63b2346066754381eeea50b6eaa211f68428
ZAID BHARDE <zaidbharde09@gmail.com>
2026-08-15 20:38:48 +0530

    Update BIO.java

--- BIO.java
+++ BIO.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Base type for every animal in the zoo. Holds shared state (name)
+ * and behavior (sleep), and forces subclasses to define their own sound.
+ */
 abstract class Animal {
+
     private final String name;
 
     protected Animal(String name) {
@@ -12,24 +17,32 @@ abstract class Animal {
         return name;
     }
 
-    abstract void makeSound();
+    /** Each concrete animal must define how it sounds. */
+    protected abstract void makeSound();
 
     public void sleep() {
         System.out.println(name + " is sleeping... Zzz");
     }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{name='" + name + "'}";
+    }
 }
 
+/** Marks an animal as domesticated / capable of playing with a human. */
 interface Pet {
     void play();
 }
 
 class Dog extends Animal implements Pet {
+
     public Dog(String name) {
         super(name);
     }
 
     @Override
-    void makeSound() {
+    protected void makeSound() {
         System.out.println(getName() + " barks: Woof! Woof!");
     }
 
@@ -40,12 +53,13 @@ class Dog extends Animal implements Pet {
 }
 
 class Cat extends Animal implements Pet {
+
     public Cat(String name) {
         super(name);
     }
 
     @Override
-    void makeSound() {
+    protected void makeSound() {
         System.out.println(getName() + " meows: Meow~");
     }
 
@@ -55,35 +69,65 @@ class Cat extends Animal implements Pet {
     }
 }
 
+class Bird extends Animal implements Pet {
+
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void makeSound() {
+        System.out.println(getName() + " chirps: Tweet tweet!");
+    }
+
+    @Override
+    public void play() {
+        System.out.println(getName() + " is fluttering around its perch.");
+    }
+}
+
 class Lion extends Animal {
+
     public Lion(String name) {
         super(name);
     }
 
     @Override
-    void makeSound() {
+    protected void makeSound() {
         System.out.println(getName() + " roars: ROAR!");
     }
 }
 
 public class BIO {
+
     public static void main(String[] args) {
+        List<Animal> animals = buildZoo();
+
+        for (Animal animal : animals) {
+            interactWith(animal);
+        }
+    }
+
+    private static List<Animal> buildZoo() {
         List<Animal> animals = new ArrayList<>();
         animals.add(new Dog("Buddy"));
         animals.add(new Cat("Luna"));
+        animals.add(new Bird("Kiwi"));
         animals.add(new Lion("Simba"));
+        return animals;
+    }
 
-        for (Animal animal : animals) {
-            System.out.println("--- Interaction with " + animal.getClass().getSimpleName() + " ---");
-            animal.makeSound();
-            animal.sleep();
+    private static void interactWith(Animal animal) {
+        System.out.println("--- Interaction with " + animal.getClass().getSimpleName() + " ---");
+        animal.makeSound();
+        animal.sleep();
 
-            if (animal instanceof Pet pet) {
-                pet.play();
-            } else {
-                System.out.println(animal.getName() + " is a wild animal and doesn't play.");
-            }
-            System.out.println();
+        if (animal instanceof Pet pet) {
+            pet.play();
+        } else {
+            System.out.println(animal.getName() + " is a wild animal and doesn't play.");
         }
+
+        System.out.println();
     }
 }
