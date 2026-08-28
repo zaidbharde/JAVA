9792d5085f525a5deaa83d5e98cf1f01ba284860
zaidbharde <zaidbharde09@gmail.com>
2026-08-28 17:29:39 +0000

    Add Java mini snippet 001

---
+++ MiniSnippet001.java
@@ -0,0 +1,6 @@
+public class MiniSnippet001 {
+    public static void main(String[] args) {
+        String word = "java-1";
+        System.out.println(new StringBuilder(word).reverse());
+    }
+}
