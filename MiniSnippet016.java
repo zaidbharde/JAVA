6bc78cb5968157956dd9bd8ebc090a2ce73fcaee
zaidbharde <zaidbharde09@gmail.com>
2026-08-28 17:29:39 +0000

    Add Java mini snippet 016

---
+++ MiniSnippet016.java
@@ -0,0 +1,6 @@
+public class MiniSnippet016 {
+    public static void main(String[] args) {
+        String word = "java-16";
+        System.out.println(new StringBuilder(word).reverse());
+    }
+}
