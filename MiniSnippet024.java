0b5844562e8674544f894360185dc303065e96d1
zaidbharde <zaidbharde09@gmail.com>
2026-08-28 17:29:39 +0000

    Add Java mini snippet 024

---
+++ MiniSnippet024.java
@@ -0,0 +1,6 @@
+public class MiniSnippet024 {
+    public static void main(String[] args) {
+        var parts = java.util.List.of("red", "blue", "green");
+        System.out.println(parts.get(0));
+    }
+}
