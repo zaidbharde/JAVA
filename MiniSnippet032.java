bfbfdf46ce24405e4d3af4dacf715bd0371bd474
zaidbharde <zaidbharde09@gmail.com>
2026-08-28 17:29:39 +0000

    Add Java mini snippet 032

---
+++ MiniSnippet032.java
@@ -0,0 +1,7 @@
+public class MiniSnippet032 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int n = 1; n <= 8; n++) total += n * n;
+        System.out.println(total);
+    }
+}
