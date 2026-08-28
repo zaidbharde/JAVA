826a4fea97f56d6f40101f1cccddd4e146a44cb0
zaidbharde <zaidbharde09@gmail.com>
2026-08-28 17:29:39 +0000

    Add Java mini snippet 026

---
+++ MiniSnippet026.java
@@ -0,0 +1,6 @@
+public class MiniSnippet026 {
+    public static void main(String[] args) {
+        String word = "java-26";
+        System.out.println(new StringBuilder(word).reverse());
+    }
+}
